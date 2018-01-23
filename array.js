56d053c42df083e74d573e2dda7dc05a67ef8fb9
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 15:33:37 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -16,7 +16,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
