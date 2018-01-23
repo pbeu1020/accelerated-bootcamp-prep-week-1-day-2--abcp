35924b25c1039eef23b2c912f4b5dc9b190523a5
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 15:31:43 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -6,6 +6,8 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
