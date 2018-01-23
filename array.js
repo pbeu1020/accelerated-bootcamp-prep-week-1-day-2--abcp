3fbbcdd6bcf0bd783e51735d0c5767085c0e5c70
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 15:38:05 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -25,7 +25,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift(element);
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
