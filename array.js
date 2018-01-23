b7b167c2a763a7aa508a84574255baec0603cb2c
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 15:43:52 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  newArray = array.slice(array.length);
+  newArray = array.slice(0, array.length-1);
   return newArray;
 }
